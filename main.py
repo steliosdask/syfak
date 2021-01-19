55ee2eff3cdbdbb0f38956fe451f0b0ba3a20279
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-01-19 12:11:16 +0200

    change df to cf(csvfile) parameter in utils.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import warnings
 from utils import *
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 df = pd.read_csv('C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\PIST_HER.CSV', delimiter=';', encoding='iso8859_7' )
 
--- utils.py
+++ utils.py
@@ -1,22 +1,18 @@
-import pandas as pd
-import warnings
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+def csvtoexcel(cf):
+    cf = cf.loc[(cf['ΠΕΡΙΓΡΑΦΗ'] == '="ΜΕΛΟΣ"') | (cf['ΠΕΡΙΓΡΑΦΗ'] == '="ΜΗ ΜΕΛΟΣ-ΦΑΡΜΑΚΕΙΟ"')]
 
-def csvtoexcel(df):
-    df = df.loc[(df['ΠΕΡΙΓΡΑΦΗ']=='="ΜΕΛΟΣ"') | (df['ΠΕΡΙΓΡΑΦΗ']== '="ΜΗ ΜΕΛΟΣ-ΦΑΡΜΑΚΕΙΟ"')]
+    cf = cf[['ΠΕΡΙΓΡΑΦΗ', 'ΚΩΔ. ΠΕΛΑΤΗ', 'ΕΠΩΝΥΜΙΑ', 'ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ', 'ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ', 'ΓΑΛΑΤΑ ΤΖΙΡΟΣ']].sort_values(['ΠΕΡΙΓΡΑΦΗ', 'ΕΠΩΝΥΜΙΑ'])
 
-    df = df[['ΠΕΡΙΓΡΑΦΗ','ΚΩΔ. ΠΕΛΑΤΗ','ΕΠΩΝΥΜΙΑ','ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ','ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ','ΓΑΛΑΤΑ ΤΖΙΡΟΣ']].sort_values(['ΠΕΡΙΓΡΑΦΗ','ΕΠΩΝΥΜΙΑ'])
-
-    df = df.drop(columns=['ΠΕΡΙΓΡΑΦΗ'])
-    df = df.drop(df.index[0])
-    df['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = df['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    df['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = df['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    df['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'] = df['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    df['ΚΩΔ. ΠΕΛΑΤΗ'] = df['ΚΩΔ. ΠΕΛΑΤΗ'].str.replace('[=,"]', '')
-    df['ΕΠΩΝΥΜΙΑ'] = df['ΕΠΩΝΥΜΙΑ'].str.replace('[=,"]', '')
+    cf = cf.drop(columns=['ΠΕΡΙΓΡΑΦΗ'])
+    cf = cf.drop(cf.index[0])
+    cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+    cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+    cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'] = cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+    cf['ΚΩΔ. ΠΕΛΑΤΗ'] = cf['ΚΩΔ. ΠΕΛΑΤΗ'].str.replace('[=,"]', '')
+    cf['ΕΠΩΝΥΜΙΑ'] = cf['ΕΠΩΝΥΜΙΑ'].str.replace('[=,"]', '')
 
 
-    df['ΣΥΝΟΛΟ'] = df.iloc[:, 2:5].sum(axis=1)
+    cf['ΣΥΝΟΛΟ'] = cf.iloc[:, 2:5].sum(axis=1)
     out_path = "C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\PIST_HER.xlsx"
-    df.to_excel(out_path, 'test.xlsx', index=False)
+    cf.to_excel(out_path, 'test.xlsx', index=False)
