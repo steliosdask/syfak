8066d22ffbfdf05e2e68b6b6f0b7823adf196586
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-01-19 13:49:22 +0200

    use lists to avoid multiple lines with same code

--- utils.py
+++ utils.py
@@ -6,13 +6,22 @@ def csvtoexcel(cf):
 
     cf = cf.drop(columns=['ΠΕΡΙΓΡΑΦΗ'])
     cf = cf.drop(cf.index[0])
-    cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'] = cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
-    cf['ΚΩΔ. ΠΕΛΑΤΗ'] = cf['ΚΩΔ. ΠΕΛΑΤΗ'].str.replace('[=,"]', '')
-    cf['ΕΠΩΝΥΜΙΑ'] = cf['ΕΠΩΝΥΜΙΑ'].str.replace('[=,"]', '')
+    titlestofloat=['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ','ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ','ΓΑΛΑΤΑ ΤΖΙΡΟΣ']
+    for i in titlestofloat:
+        cf[i] = cf[i].str.replace(',', '.').astype(float)
+    titlesvalues = ['ΚΩΔ. ΠΕΛΑΤΗ','ΕΠΩΝΥΜΙΑ']
+    for j in titlesvalues:
+        cf[j] = cf[j].str.replace('[=,"]', '')
+
 
 
     cf['ΣΥΝΟΛΟ'] = cf.iloc[:, 2:5].sum(axis=1)
     out_path = "C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\PIST_HER.xlsx"
     cf.to_excel(out_path, index=False)
+
+    ''' cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+        cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'] = cf['ΠΑΡΑΦΑΡΜΑΚΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+        cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'] = cf['ΓΑΛΑΤΑ ΤΖΙΡΟΣ'].str.replace(',', '.').astype(float)
+         
+        cf['ΚΩΔ. ΠΕΛΑΤΗ'] = cf['ΚΩΔ. ΠΕΛΑΤΗ'].str.replace('[=,"]', '')
+        cf['ΕΠΩΝΥΜΙΑ'] = cf['ΕΠΩΝΥΜΙΑ'].str.replace('[=,"]', '')'''
