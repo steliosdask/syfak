36fb9dffc325740a7a0d3e7cec1b8ac8904a64b8
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-01-26 11:39:50 +0200

    easier to understant list names

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from utils import *
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-files = ['PIST_HER','PIST_AIG','PIST_LAS','PIST_RHO','PIST_RET']
-files2 = ['ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΗΡΑΚΛΕΙΟ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΑΙΓΑΙΟ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΛΑΣΙΘΙ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΡΟΔΟΣ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΡΕΘΥΜΝΟ']
-for num in range(len(files)):
-    df = pd.read_csv('C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\' + files[num] +'.CSV', delimiter=';', encoding='iso8859_7' )
+startfiles = ['PIST_HER','PIST_AIG','PIST_LAS','PIST_RHO','PIST_RET']
+finalfiles = ['ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΗΡΑΚΛΕΙΟ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΑΙΓΑΙΟ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΛΑΣΙΘΙ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΡΟΔΟΣ','ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ ΡΕΘΥΜΝΟ']
+
+
+for num in range(len(startfiles)):
+    df = pd.read_csv('C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\' + startfiles[num] +'.CSV', delimiter=';', encoding='iso8859_7' )
 
     newfile = csvtoexcel(df)
 
 
-    out_path = 'C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\' + files2[num] + '.xlsx'
+    out_path = 'C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\' + finalfiles[num] + '.xlsx'
     newfile.to_excel(out_path, index=False)
 
 
