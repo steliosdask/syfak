58129e11400628e153d3f4e0f2f712a08eecb285
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-01-26 11:07:22 +0200

    add a list for multiple files

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pandas as pd
 import warnings
 from utils import *
 
-
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+files= ['PIST_HER','PIST_AIG','PIST_LAS','PIST_RHO','PIST_RET']
+for file in files:
+    df = pd.read_csv('C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\' + file +'.CSV', delimiter=';', encoding='iso8859_7' )
+    out_path = 'C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\' + file +'.xlsx'
+    newfile = csvtoexcel(df)
+    newfile.to_excel(out_path, index=False)
 
-df = pd.read_csv('C:\\Users\StelD\\Documents\\GitHub\\syfak\\files\\PIST_HER.CSV', delimiter=';', encoding='iso8859_7' )
-out_path = "C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\PIST_HER.xlsx"
 
-
-newfile = csvtoexcel(df)
-newfile.to_excel(out_path, index=False)
 
