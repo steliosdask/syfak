2b98fbe0c92c12fa4b2bf705959578f2e18f87a6
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-03-08 13:28:41 +0200

    new way to create new files to xlsx

--- main.py
+++ main.py
@@ -19,10 +19,46 @@ if checkinput == 'Y' or checkinput == 'Yes' or checkinput == 'y' or checkinput =
     for file in files_new:
         df = pd.read_csv(path + '\\'+ file, delimiter=';', encoding='iso8859_7')
         xlsx_file = csvtoexcel(df)
-        print(xlsx_file)
-        #out_path = 'C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\' + finalfiles[num] + '.xlsx'
-        #xlsx_file.to_excel(out_path, index=False)
+        namecheck = file.split('_')
+        st1 = "ΤΖΙΡΟΙ ΓΙΑ ΠΙΣΤΩΤΙΚΑ "
+        st2 = ""
+        st3 = ""
+        if len(namecheck) < 3:
+            if namecheck[1] == 'AIG.CSV':
+                st2 = "ΑΙΓΑΙΟ"
+            elif namecheck[1] == 'HER.CSV':
+                st2 = "ΗΡΑΚΛΕΙΟ"
+            elif namecheck[1] == 'RHO.CSV':
+                st2 = "ΡΟΔΟΣ"
+            elif namecheck[1] == 'RET.CSV':
+                st2 = "ΡΕΘΥΜΝΟ"
+            elif namecheck[1] == 'LAS.CSV':
+                st2 = "ΛΑΣΙΘΙ"
+            else :
+                print("WRONG FILE!?")
+        else:
+            if namecheck[1] == 'AIG':
+                st2 = "ΑΙΓΑΙΟ"
+                st3 = " 2"
+            elif namecheck[1] == 'HER':
+                st2 = "ΗΡΑΚΛΕΙΟ"
+                st3 = " 2"
+            elif namecheck[1] == 'RHO':
+                st2 = "ΡΟΔΟΣ"
+                st3 = " 2"
+            elif namecheck[1] == 'RET':
+                st2 = "ΡΕΘΥΜΝΟ"
+                st3 = " 2"
+            elif namecheck[1] == 'LAS':
+                st2 = "ΛΑΣΙΘΙ"
+                st3 = " 2"
+            else :
+                print("WRONG FILE!?")
 
+        out_path = 'C:\\Users\\StelD\\Documents\\GitHub\\syfak\\files\\' +st1 + st2 +st3 +'.xlsx'
+        xlsx_file.to_excel(out_path, index=False)
+
+print('You are ready!')
 
 
 '''startfiles = ['PIST_HER','PIST_AIG','PIST_LAS','PIST_RHO','PIST_RET','PIST_HER2','PIST_AIG2','PIST_LAS2','PIST_RHO2','PIST_RET2']
