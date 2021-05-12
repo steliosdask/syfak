88ef1ba42dfc9d3ebc5a65286af0e1bae0a05c51
steliosdask <56888548+steliosdask@users.noreply.github.com>
2021-05-12 10:52:24 +0300

    no print on screen

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from utils import *
 
 namelist = listOfFiles()
 
-print("This is the list of csv files! proceed to xlsx form? \n", namelist)
+#print("This is the list of csv files! proceed to xlsx form? \n", namelist)
 #checkinput = input()
 
 finalExcelName()
 
-print('You are ready!')
+#print('You are ready!')
 
 
 
